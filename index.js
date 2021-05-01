e62679a0cb4067beb6fede95294c2fff47a706db
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-01 23:24:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var myArr = JSON.parse(this.responseText);
-//         document.getElementById("demo").innerHTML = myArr[name];
-//     }
-// };
-// xmlhttp.open("GET", "list.json", true);
-// console.log(xmlhttp);
-// xmlhttp.send();
+fetch("list.json", { mode: "no-cors" }) // disable CORS because path does not contain http(s)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
 
-var requestURL = 'https://github.com/Masianuy/Soft/blob/main/list.json';
-console.log(requestURL.responseText);
+// var requestURL = 'https://github.com/Masianuy/Soft/blob/main/list.json';
+// console.log(requestURL.responseText);
