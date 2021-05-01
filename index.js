130e7b23da11a00213ada7184c7e191641e58043
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-01 23:18:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myArr[name];
-    }
-};
-xmlhttp.open("GET", "list.json", true);
-console.log(xmlhttp);
-xmlhttp.send();
+// var xmlhttp = new XMLHttpRequest();
+// xmlhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         var myArr = JSON.parse(this.responseText);
+//         document.getElementById("demo").innerHTML = myArr[name];
+//     }
+// };
+// xmlhttp.open("GET", "list.json", true);
+// console.log(xmlhttp);
+// xmlhttp.send();
+
+var requestURL = 'https://github.com/Masianuy/Soft/blob/main/list.json';
+console.log(requestURL.name);
