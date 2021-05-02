5b9d3e8a4897f3a7dc9f6d6612bbc51d9708271f
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-02 15:11:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myArr['1'].name;
+        for (let i=0; i<myArr.length; i++) {
+            document.getElementById("demo").innerHTML = myArr[i].name;
+        }
+//         document.getElementById("demo").innerHTML = myArr['1'].name;
     }
 };
 xmlhttp.open("GET", "list.json", true);
 xmlhttp.send();
-
