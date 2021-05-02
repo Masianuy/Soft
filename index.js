13b471ee603426ed9d83ad366cace5d5010f2f1d
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-02 19:39:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function startJson () {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const myArr = JSON.parse(this.responseText);
+            // let output = "";
+            // for (let i=0; i<myArr.length; i++) {
+            //     output += '<li>' + myArr[i].name + '</li>';
+            // }
+            // document.getElementById("demo").innerHTML = output;
+            let firstLetterName = myArr[i].lower.slice(0,1);
+            console.log(firstLetterName);
+        }
+    };
+    xmlhttp.open("GET", "list.json", true);
+    xmlhttp.send();
+}
+    
 randomString();
 function randomString() {
             //define a variable consisting alphabets in small and capital letter
@@ -11,41 +29,23 @@ function randomString() {
 	for (let i=0; i<lenString; i++) {
 		let rnum = Math.floor(Math.random() * characters.length);
 		randomstring += '<li>' + characters.substring(rnum, rnum+1) + '</li>';
-	}
-
-             //display the generated string 
+	} 
 	document.getElementById("randomfield").innerHTML = randomstring;
 }
 
 const listLetters = document.getElementById('randomfield');
-const letter = listLetters.querySelectorAll('#randomfield li');
+const letter = listLetters.querySelectorAll('li');
 
 listLetters.addEventListener('click', e => {
 
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        let myArr = JSON.parse(xmlhttp);
-
-        let output = "";
-        for (let i=0; i<myArr.length; i++) {
-            output += '<li>' + myArr[i].name + '</li>';
-        }
-        document.getElementById("demo").innerHTML = output;
-        
-    };
-    xmlhttp.open("GET", "list.json", true);
-    xmlhttp.send();
-
     const target = e.target;
-
     let q = target.innerHTML;
-        console.log(q);
     let firstLetterName = myArr[i].lower.slice(0,1);
-        console.log(firstLetterName);
-
-    if(firstLetterName == q) {
-        console.log('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq');
-    }
+    
+    startJson ()
 
 });
 
+// if (myArr.indexOf() === q) {
+
+// }
