ea0515ab8aff96acd551b919ff5433dd2a632ec6
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-02 23:56:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ listLetters.addEventListener('click', e => {
         removeActiveElement();
         target.classList.add('activ'); 
         let targetInner = target.innerHTML;
-        function startJson () {
+        function     {
             let xmlhttp = new XMLHttpRequest();
             xmlhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
@@ -62,7 +62,8 @@ listLetters.addEventListener('click', e => {
             xmlhttp.send();
         }
         }
-});
+        startJson ()
+    });
 const removeActiveElement = () => {
     letter.forEach(elem => elem.classList.remove('activ'));
 } 
