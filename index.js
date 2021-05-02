6e9750e8b31cd0688366035439ffeb051fde0d34
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-02 15:33:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "list.json", true);
 xmlhttp.send();
+
+
+function randomString() {
+            //define a variable consisting alphabets in small and capital letter
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+            
+            //specify the length for the new string
+	let lenString = 5;
+	let randomstring = '';
+
+            //loop to select a new character in each iteration
+	for (let i=0; i<lenString; i++) {
+		let rnum = Math.floor(Math.random() * characters.length);
+		randomstring += characters.substring(rnum, rnum+1);
+	}
+
+             //display the generated string 
+	document.getElementById("randomfield").innerHTML = randomstring;
+}
