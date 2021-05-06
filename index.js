a590f146c920493dd3382248edde0fa55ff3ba30
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-06 21:29:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const letter = listLetters.querySelectorAll('option');
 
 listLetters.addEventListener('click', e => {
     const target = e.target;
-    document.getElementById("randomfield").reset();
+    document.getElementById("randomfield").reset(e);
     if(target.matches('option')) {
         removeActiveElement();
         target.classList.add('activ'); 
