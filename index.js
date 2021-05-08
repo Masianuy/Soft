c15fe684737db5b79112de7505fd9e1d06935de8
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-08 23:38:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ listLetters.addEventListener('click', e => {
                             let li = document.createElement('li');
                             li.innerText = myArr[i].name;
                             ul.appendChild(li);
-                        }  else {
+                        }  if else (myArr[i].name[0] != targetInner) {
                             break;
                             let span = document.createElement('span');
                             span.innerText = 'Error';
