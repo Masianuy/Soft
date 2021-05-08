8e8f9b1876adde50e3bda54f3ed8ef50f419c3ef
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-08 11:03:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ listLetters.addEventListener('click', e => {
                     for (let i=0; i<myArr.length; i++) {
                         if (myArr[i].name.substr(0,1) === targetInner) {
                             let ul = document.getElementById("demo");
-                            while (ul.firstChild) {
-                              ul.removeChild(ul.firstChild);
-                            }
+                            ul.removeChild(li);
                             let li = document.createElement('li');
                             li.innerText = myArr[i].name;
                             ul.appendChild(li);
