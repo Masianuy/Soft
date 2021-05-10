91bacea1115502c4ebee5dfd448228d45ed5830d
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-10 22:59:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function randomString() {
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
     let randomstring = '';
     for (let i=0; i<5; i++) {
-        let rnum = Math.floor((Math.random() * characters.length)+1);
+        let rnum = Math.floor(Math.random() * characters.length);
         randomstring += '<option>' + characters.substring(rnum, rnum+1) + '</option>';
     } 
     document.getElementById("randomfield").innerHTML = randomstring;
