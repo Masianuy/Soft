5df8b0f1f7d73f45191f6af814a67a8293d823eb
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-14 23:41:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+var myList = document.querySelector('ul');
+
+var myRequest = new Request('list.json');
+
+fetch(myRequest)
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    for (var i = 0; i < data.products.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.innerHTML = '<strong>' + data.products[i].Name + '</strong> can be found in ' +
+                           data.products[i].Location +
+                           '. Cost: <strong>£' + data.products[i].Price + '</strong>';
+      myList.appendChild(listItem);
+    }
+  });
+
 
 randomString();
 function randomString() {
