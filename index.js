a0526c10dc0eca1f67f4e87c004b86c8392187df
Masianuy <39313328+Masianuy@users.noreply.github.com>
2021-05-15 21:15:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
 randomString();
 function randomString() {
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
+    let lenString = 5;
     let randomstring = '';
-    for (let i=0; i<5; i++) {
+    for (let i=0; i<lenString; i++) {
         let rnum = Math.floor(Math.random() * characters.length);
         randomstring += '<option>' + characters.substring(rnum, rnum+1) + '</option>';
     } 
@@ -12,49 +12,23 @@ function randomString() {
 
 const listLetters = document.getElementById('randomfield');
 const letter = listLetters.querySelectorAll('option');
-const ul = document.getElementById("demo");
 
-listLetters.addEventListener('click', e => {
-    const target = e.target;
-    if(target.matches('option')) {
-        removeActiveElement();
-        target.classList.add('activ');
-
+let request = new XMLHttpRequest();
+    request.open("GET", "list.json", true);
+      request.onload = function () {
+      let users = JSON.parse(this.response);
+      let output = '';
+      listLetters.addEventListener('click', e => {
+        const target = e.target;
         let targetInner = target.innerHTML;
-        function startJson () {
-            let xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    const myArr = JSON.parse(this.responseText);
-                    delLi();
-                    function delLi() {
-                        let lit = document.querySelectorAll('li');
-                        for (let i = 0; i<lit.length; i++) {
-                            lit[i].classList.add('hide');
-                        }
-                    };
-                    for (let i=0; i<myArr.length; i++) {
-                        if (myArr[i].name[0] == targetInner) {
-                            let li = document.createElement('li');
-                            li.innerText = myArr[i].name;
-                            ul.appendChild(li);
-                        } else if (myArr[i].name[0] ==! targetInner) {
-                            break;
-                            let span = document.createElement('span');
-                            span.innerText = 'Error';
-                            ul.appendChild(span);
-                        };
-                    };
-                }
-            };
-            xmlhttp.open("GET", "list.json", true);
-            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xmlhttp.send();
+        for (let i=0; i<users.length; i++) {
+            if (users[i].name[0] == targetInner) {
+                let li = document.createElement('li');
+                li.innerText = users[i].name;
+                // ul.appendChild(li);
+            }
+            document.getElementById('demo').innerHTML = output;
         }
-        
-        startJson ();
+      })
     }
-});
-const removeActiveElement = () => {
-    letter.forEach(elem => elem.classList.remove('activ'));
-} 
+    request.send();
